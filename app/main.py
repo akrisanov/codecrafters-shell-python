c57e102134c9e54da3918fb33f9528d3499b2335
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2026-03-23 16:00:31 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 def main():
     while True:
-        command = input("$ ")
-        if command == "exit":
-            exit()
+        command, args = prepare(input("$ "))
+        handle(command, args)
+
+
+def prepare(command: str) -> tuple[str, list[str]]:
+    parts = command.split()
+    return parts[0], parts[1:]
+
+
+def handle(command: str, args: list[str] | None = None):
+    if command == "exit":
+        exit()
+    else:
         print(f"{command}: command not found")
 
 
